32936abb1ff760d64b0277db545c749088d845d7
c4bhau <72193284+Alexanderdgg@users.noreply.github.com>
2024-02-14 10:24:33 +0100

    first steps

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,29 @@
+using FluentAssertions;
+
 namespace TestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void RangeCheck_Roemisch_1_To_9()
         {
-
+            GetRoemisch(0).Should().Be("error");
         }
+
+        string GetRoemisch(int i)
+        {
+            return "";
+        }
+        /*
+         * I
+         * II
+         * III
+         * V
+         * X
+         * L
+         * C
+         * D
+         * M
+         */
     }
 }
