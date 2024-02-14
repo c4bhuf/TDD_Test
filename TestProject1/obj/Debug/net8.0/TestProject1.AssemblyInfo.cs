c7402ec0d8c23803887d10ca21e4f558d20f34e2
c4bhau <72193284+Alexanderdgg@users.noreply.github.com>
2024-02-14 10:29:37 +0100

    Squashed commit of the following:
    commit 473ef13d90c1ab1166da25e73b311f54a6ebf67f
    Merge: 32936ab eeb7427
    Author: c4bhau <72193284+Alexanderdgg@users.noreply.github.com>
    Date:   Wed Feb 14 10:25:09 2024 +0100
    
        Merge branch 'mob/main' of https://github.com/c4bhuf/TDD_Test into mob/main
    
    commit 32936abb1ff760d64b0277db545c749088d845d7
    Author: c4bhau <72193284+Alexanderdgg@users.noreply.github.com>
    Date:   Wed Feb 14 10:24:33 2024 +0100
    
        first steps

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41412b1342232b9fb4ffbad5dfeb6441edf3b110")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
